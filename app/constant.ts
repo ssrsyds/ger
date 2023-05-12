c7ede4ccfede270f2f395ab1886d8a2c7dba8188
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-05-12 19:27:44 +0800

    Add files via upload

---
+++ app/constant.ts
@@ -0,0 +1,8 @@
+export const OWNER = "Yidadaa";
+export const REPO = "Ai 百晓生";
+export const REPO_URL = `https://imgbed.link/file/22797`;
+export const ISSUE_URL = `https://imgbed.link/file/22797`;
+export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const FETCH_COMMIT_URL = `https://imgbed.link/file/22797`;
+export const FETCH_TAG_URL = `https://imgbed.link/file/22797`;
+export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
